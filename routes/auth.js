3b5bf2dddb03be238e70bbffc28a3a095167e5d3
dghero <devingreyhero@gmail.com>
2018-08-15 13:16:50 -0500

    Add missed files for user auth

---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+//authentication
+const User = require('../models/user');
+const passport = require('passport');
+const options = {session: false, failWithError: true};
+const localAuth = passport.authenticate('local', options);
+
+const {JWT_SECRET, JWT_EXPIRY} = require('../config');
+const jwt = require('jsonwebtoken');
+
+//init router
+const router = express.Router();
+
+//Auth token generator
+function createAuthToken (user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
+
+const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
+
+router.post('/refresh', jwtAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  res.json({ authToken });
+});
+
+//Authenticate user
+router.post('/', localAuth, function (req, res) {
+  const authToken = createAuthToken(req.user);
+  return res.json({authToken});
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/', (req,res,next) =>{
+  const newObj = {};
+  const inputFields = ['username', 'password'];
+  const authFields = ['username', 'password'];
+
+  inputFields.forEach(field =>{
+    if(field in req.body)
+      newObj[field] = req.body[field];
+  });
+
+  /* Trust not the user, for they will lead you astray */
+
+  inputFields.forEach(field =>{
+    if(typeof newObj[field] !== 'string' && newObj[field]){
+      const err = new Error(`Field \`${field}\` is not a string`);
+      err.status = 400;
+      return next(err);
+    }
+  });
+
+  authFields.forEach(field =>{
+    if(!newObj[field]){
+      const err = new Error(`Missing \`${field}\` in request body`);
+      err.status = 400;
+      return next(err);
+    }
+
+    if(newObj[field].charAt(0) === ' ' || newObj[field].charAt(newObj[field].length-1) === ' '){
+      const err = new Error(`Leading or trailing whitespace in \`${field}\``);
+      err.status = 400;
+      return next(err);
+    }
+  });
+
+  if(newObj.username < 1){
+    const err = new Error('`username` must contain at least 1 character)');
+      err.status = 400;
+      return next(err);
+  }
+
+  if(newObj.password.length < 8 || newObj.password.length > 72){
+    const err = new Error('`password` must be between 8 and 72 characters');
+      err.status = 400;
+      return next(err);    
+  }
+
+  return User.hashPassword(newObj.password)
+    .then(digest => {
+      const newUser = {
+        username: newObj.username,
+        password: digest,
+      };
+      return User.create(newUser);
+    })
+    .then(results =>{
+      res.location(`${req.originalUrl}/${results.id}`).status(201).json(results);
+    })
+    .catch(err =>{
+      if(err.code === 11000){
+        err = new Error('The username already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
+
+});
+
+module.exports = router;
